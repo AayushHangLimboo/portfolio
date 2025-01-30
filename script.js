3365547d06363575f143d70c64b8806eb199a926
AayushHangLimboo <aayushlimboo12345@gmail.com>
2025-01-30 21:46:45 +0530

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("btn").addEventListener("click",() =>{
+    const fact = document.getElementById("fun-fact-text");
+
+    if(fact.style.display==="none" ){
+        fact.style.display="block";
+    }
+    else{
+        fact.style.display="none";
+    }
+})
